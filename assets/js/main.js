70452d9eb974eec829c0ed6ddcad81942b8b55fa
sedano <alejandro@sedano.me>
2017-12-26 15:04:04 -0600

    Add vivero content

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,39 +9,39 @@ var settings = {
 	banner: {
 
 		// Indicators (= the clickable dots at the bottom).
-			indicators: true,
+		indicators: true,
 
 		// Transition speed (in ms)
 		// For timing purposes only. It *must* match the transition speed of "#banner > article".
-			speed: 1500,
+		speed: 1500,
 
 		// Transition delay (in ms)
-			delay: 5000,
+		delay: 5000,
 
 		// Parallax intensity (between 0 and 1; higher = more intense, lower = less intense; 0 = off)
-			parallax: 0.25
+		parallax: 0.25
 
 	}
 
 };
 
-(function($) {
+(function ($) {
 
 	skel.breakpoints({
-		xlarge:	'(max-width: 1680px)',
-		large:	'(max-width: 1280px)',
-		medium:	'(max-width: 980px)',
-		small:	'(max-width: 736px)',
-		xsmall:	'(max-width: 480px)'
+		xlarge: '(max-width: 1680px)',
+		large: '(max-width: 1280px)',
+		medium: '(max-width: 980px)',
+		small: '(max-width: 736px)',
+		xsmall: '(max-width: 480px)'
 	});
 
 	/**
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
 	 */
-	$.fn._parallax = (skel.vars.browser == 'ie' || skel.vars.mobile) ? function() { return $(this) } : function(intensity) {
+	$.fn._parallax = (skel.vars.browser == 'ie' || skel.vars.mobile) ? function () { return $(this) } : function (intensity) {
 
-		var	$window = $(window),
+		var $window = $(window),
 			$this = $(this);
 
 		if (this.length == 0 || intensity === 0)
@@ -49,7 +49,7 @@ var settings = {
 
 		if (this.length > 1) {
 
-			for (var i=0; i < this.length; i++)
+			for (var i = 0; i < this.length; i++)
 				$(this[i])._parallax(intensity);
 
 			return $this;
@@ -59,17 +59,17 @@ var settings = {
 		if (!intensity)
 			intensity = 0.25;
 
-		$this.each(function() {
+		$this.each(function () {
 
 			var $t = $(this),
 				on, off;
 
-			on = function() {
+			on = function () {
 
 				$t.css('background-position', 'center 100%, center 100%, center 0px');
 
 				$window
-					.on('scroll._parallax', function() {
+					.on('scroll._parallax', function () {
 
 						var pos = parseInt($window.scrollTop()) - parseInt($t.position().top);
 
@@ -79,7 +79,7 @@ var settings = {
 
 			};
 
-			off = function() {
+			off = function () {
 
 				$t
 					.css('background-position', '');
@@ -89,7 +89,7 @@ var settings = {
 
 			};
 
-			skel.on('change', function() {
+			skel.on('change', function () {
 
 				if (skel.breakpoint('medium').active)
 					(off)();
@@ -102,7 +102,7 @@ var settings = {
 
 		$window
 			.off('load._parallax resize._parallax')
-			.on('load._parallax resize._parallax', function() {
+			.on('load._parallax resize._parallax', function () {
 				$window.trigger('scroll');
 			});
 
@@ -114,9 +114,9 @@ var settings = {
 	 * Custom banner slider for Slate.
 	 * @return {jQuery} jQuery object.
 	 */
-	$.fn._slider = function(options) {
+	$.fn._slider = function (options) {
 
-		var	$window = $(window),
+		var $window = $(window),
 			$this = $(this);
 
 		if (this.length == 0)
@@ -124,7 +124,7 @@ var settings = {
 
 		if (this.length > 1) {
 
-			for (var i=0; i < this.length; i++)
+			for (var i = 0; i < this.length; i++)
 				$(this[i])._slider(options);
 
 			return $this;
@@ -132,184 +132,214 @@ var settings = {
 		}
 
 		// Vars.
-			var	current = 0, pos = 0, lastPos = 0,
-				slides = [], indicators = [],
-				$indicators,
-				$slides = $this.children('article'),
-				intervalId,
-				isLocked = false,
-				i = 0;
+		var current = 0, pos = 0, lastPos = 0,
+			slides = [], indicators = [],
+			$indicators,
+			$slides = $this.children('article'),
+			intervalId,
+			isLocked = false,
+			i = 0;
 
 		// Turn off indicators if we only have one slide.
-			if ($slides.length == 1)
-				options.indicators = false;
+		if ($slides.length == 1)
+			options.indicators = false;
+		else
+			$slides = $slides.slice(1);
 
 		// Functions.
-			$this._switchTo = function(x, stop) {
+		$this._switchTo = function (x, stop) {
 
-				if (isLocked || pos == x)
-					return;
+			if (isLocked || pos == x)
+				return;
 
-				isLocked = true;
+			isLocked = true;
 
-				if (stop)
-					window.clearInterval(intervalId);
+			if (stop)
+				window.clearInterval(intervalId);
 
-				// Update positions.
-					lastPos = pos;
-					pos = x;
+			// Update positions.
+			lastPos = pos;
+			pos = x;
 
-				// Hide last slide.
-					slides[lastPos].removeClass('top');
+			// Hide last slide.
+			slides[lastPos].removeClass('top');
 
-					if (options.indicators)
-						indicators[lastPos].removeClass('visible');
-
-				// Show new slide.
-					slides[pos].addClass('visible').addClass('top');
-
-					if (options.indicators)
-						indicators[pos].addClass('visible');
-
-				// Finish hiding last slide after a short delay.
-					window.setTimeout(function() {
-
-						slides[lastPos].addClass('instant').removeClass('visible');
-
-						window.setTimeout(function() {
-
-							slides[lastPos].removeClass('instant');
-							isLocked = false;
-
-						}, 100);
-
-					}, options.speed);
-
-			};
-
-		// Indicators.
 			if (options.indicators)
-				$indicators = $('<ul class="indicators"></ul>').appendTo($this);
+				indicators[lastPos].removeClass('visible');
 
-		// Slides.
-			$slides
-				.each(function() {
-
-					var $slide = $(this),
-						$img = $slide.find('img');
-
-					// Slide.
-						$slide
-							.css('background-image', 'url("' + $img.attr('src') + '")')
-							.css('background-position', ($slide.data('position') ? $slide.data('position') : 'center'));
-
-					// Add to slides.
-						slides.push($slide);
-
-					// Indicators.
-						if (options.indicators) {
-
-							var $indicator_li = $('<li>' + i + '</li>').appendTo($indicators);
-
-							// Indicator.
-								$indicator_li
-									.data('index', i)
-									.on('click', function() {
-										$this._switchTo($(this).data('index'), true);
-									});
-
-							// Add to indicators.
-								indicators.push($indicator_li);
-
-						}
-
-					i++;
-
-				})
-				._parallax(options.parallax);
-
-		// Initial slide.
+			// Show new slide.
 			slides[pos].addClass('visible').addClass('top');
 
 			if (options.indicators)
 				indicators[pos].addClass('visible');
 
+			// Finish hiding last slide after a short delay.
+			window.setTimeout(function () {
+
+				slides[lastPos].addClass('instant').removeClass('visible');
+
+				window.setTimeout(function () {
+
+					slides[lastPos].removeClass('instant');
+					isLocked = false;
+
+				}, 100);
+
+			}, options.speed);
+
+		};
+
+		// Indicators.
+		if (options.indicators)
+			$indicators = $('<ul class="indicators"></ul>').appendTo($this);
+
+		// Slides.
+		$slides
+			.each(function () {
+
+				var $slide = $(this),
+					$img = $slide.find('img');
+
+				// Slide.
+				$slide
+					.css('background-image', 'url("' + $img.attr('src') + '")')
+					.css('background-position', ($slide.data('position') ? $slide.data('position') : 'center'));
+
+				// Add to slides.
+				slides.push($slide);
+
+				// Indicators.
+				if (options.indicators) {
+
+					var $indicator_li = $('<li>' + i + '</li>').appendTo($indicators);
+
+					// Indicator.
+					$indicator_li
+						.data('index', i)
+						.on('click', function () {
+							$this._switchTo($(this).data('index'), true);
+						});
+
+					// Add to indicators.
+					indicators.push($indicator_li);
+
+				}
+
+				i++;
+
+			})
+			._parallax(options.parallax);
+
+		// Initial slide.
+		slides[pos].addClass('visible').addClass('top');
+
+		if (options.indicators)
+			indicators[pos].addClass('visible');
+
 		// Bail if we only have a single slide.
-			if (slides.length == 1)
-				return;
+		if (slides.length == 1)
+			return;
 
 		// Main loop.
-			intervalId = window.setInterval(function() {
+		intervalId = window.setInterval(function () {
 
-				current++;
+			current++;
 
-				if (current >= slides.length)
-					current = 0;
+			if (current >= slides.length)
+				current = 0;
 
-				$this._switchTo(current);
+			$this._switchTo(current);
 
-			}, options.delay);
+		}, options.delay);
 
 	};
 
-	$(function() {
+	$(function () {
 
-		var	$window 	= $(window),
-			$body 		= $('body'),
-			$header 	= $('#header'),
-			$banner 	= $('.banner');
+		var $window = $(window),
+			$body = $('body'),
+			$header = $('#header'),
+			$banner = $('.banner');
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
+		$body.addClass('is-loading');
 
-			$window.on('load', function() {
-				window.setTimeout(function() {
-					$body.removeClass('is-loading');
-				}, 100);
-			});
+		$window.on('load', function () {
+			window.setTimeout(function () {
+				$body.removeClass('is-loading');
+			}, 100);
+		});
 
 		// Prioritize "important" elements on medium.
-			skel.on('+medium -medium', function() {
-				$.prioritize(
-					'.important\\28 medium\\29',
-					skel.breakpoint('medium').active
-				);
-			});
+		skel.on('+medium -medium', function () {
+			$.prioritize(
+				'.important\\28 medium\\29',
+				skel.breakpoint('medium').active
+			);
+		});
 
 		// Banner.
-			$banner._slider(settings.banner);
+		$banner._slider(settings.banner);
 
 		// Menu.
-			$('#menu')
-				.append('<a href="#menu" class="close"></a>')
-				.appendTo($body)
-				.panel({
-					delay: 500,
-					hideOnClick: true,
-					hideOnSwipe: true,
-					resetScroll: true,
-					resetForms: true,
-					side: 'right'
-				});
+		$('#menu')
+			.append('<a href="#menu" class="close"></a>')
+			.appendTo($body)
+			.panel({
+				delay: 500,
+				hideOnClick: true,
+				hideOnSwipe: true,
+				resetScroll: true,
+				resetForms: true,
+				side: 'right'
+			});
 
 		// Header.
-			if (skel.vars.IEVersion < 9)
-				$header.removeClass('alt');
+		if (skel.vars.IEVersion < 9)
+			$header.removeClass('alt');
 
-			if ($banner.length > 0
-			&&	$header.hasClass('alt')) {
+		if ($banner.length > 0
+			&& $header.hasClass('alt')) {
 
-				$window.on('resize', function() { $window.trigger('scroll'); });
+			$window.on('resize', function () { $window.trigger('scroll'); });
 
-				$banner.scrollex({
-					bottom:		$header.outerHeight(),
-					terminate:	function() { $header.removeClass('alt'); },
-					enter:		function() { $header.addClass('alt'); },
-					leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
-				});
+			$banner.scrollex({
+				bottom: $header.outerHeight(),
+				terminate: function () { $header.removeClass('alt'); },
+				enter: function () { $header.addClass('alt'); },
+				leave: function () { $header.removeClass('alt'); $header.addClass('reveal'); }
+			});
 
-			}
+		}
 
 	});
 
+	// Scrolly links.
+	$('.scrolly').scrolly();
+
+	// Nav.
+	var $nav_a = $('#nav a');
+
+	// Scrolly-fy links.
+	$nav_a
+		.scrolly()
+		.on('click', function (e) {
+
+			var t = $(this),
+				href = t.attr('href');
+
+			if (href[0] != '#')
+				return;
+
+			e.preventDefault();
+
+			// Clear active and lock scrollzer until scrolling has stopped
+			$nav_a
+				.removeClass('active')
+				.addClass('scrollzer-locked');
+
+			// Set this link to active
+			t.addClass('active');
+
+		});
 })(jQuery);
